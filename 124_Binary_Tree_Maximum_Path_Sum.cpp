9d583c2115d5975a3b0f3097a69755917d6ddf40
LucaWei <lucawei@moldex3d.com>
2022-10-05 21:47:47 +0800

    add file

--- 124_Binary_Tree_Maximum_Path_Sum.cpp
+++ 124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -11,20 +11,24 @@
  */
 
 
-int PathSum(TreeNode* root, int& ans) {
-    if (root == nullptr) {
-        return 0;
-    }
-    int left = std::max(0, PathSum(root->left, ans));
-    int right = std::max(0, PathSum(root->right, ans));
-    ans = std::max(ans, left + right + root->val); 
-    return std::max(left, right) + root->val;
-}
+
 class Solution {
-public:
+ public:
     int maxPathSum(TreeNode* root) {
-        int ans = std::numeric_limits<int>::min();
-        PathSum(root, ans);
-        return ans;
+        ans_ = std::numeric_limits<int>::min();
+        PathSum(root);
+        return ans_;
+    }
+ private:
+    int ans_ = std::numeric_limits<int>::min();
+    int PathSum(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left = std::max(0, PathSum(root->left));
+        int right = std::max(0, PathSum(root->right));
+        ans_ = std::max(ans_, left + right + root->val);
+        std::cout << "Ans= " << left + right + root->val << std::endl;
+        return std::max(left, right) + root->val;
     }
 };
