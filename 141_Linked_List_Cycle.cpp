a1b83da136a1c950fd3ca1e5f7f81deedeffd7e5
Luca Wei <48273316+HSUAN221@users.noreply.github.com>
2022-10-11 20:10:59 +0800

    Create 141_Linked_List_Cycle.cpp

---
+++ 141_Linked_List_Cycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        std::unordered_set<ListNode*> visited;
+        while (head) {
+            if (visited.count(head)) {
+                return true;
+            }
+            visited.insert(head);
+            head = head->next;
+        }
+        return false;
+    }
+};
