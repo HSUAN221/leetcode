399413a53e6ee584d298e7df779df6d5746efbbe
Luca Wei <48273316+HSUAN221@users.noreply.github.com>
2022-11-05 00:01:28 +0800

    Create 100_Same_Tree.cpp

---
+++ 100_Same_Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+ private:
+    bool isSame_ = true;
+    void preorder(TreeNode* p, TreeNode* q) {
+        if (p != nullptr && q != nullptr) {
+            if (p->val != q->val) {
+                isSame_ = false;
+            }
+            preorder(p->left, q->left);
+            preorder(p->right, q->right);
+        } else if (p != nullptr && q == nullptr) {
+            isSame_ = false;
+        } else if (p == nullptr && q != nullptr) {
+            isSame_ = false;
+        }
+    }
+ public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        preorder(p, q);
+        return isSame_;
+    }
+};
